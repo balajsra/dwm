0a4ab3b407df71e5fc8d2f16ed27e06c8c4e10c8
Sravan Balaji <balajsra@umich.edu>
2020-07-26 18:45:58 -0400

    Move rclone out of autostart_blocking
    - Put rclone sync in autostart so dwm isn't prevented from starting

---
+++ autostart.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Local cloud storage directory
+local_clone_dir="$HOME/Cloud"
+
+# List of remotes as defined in rclone
+declare -a remote_array=(\
+    "OneDrive - Personal" \
+    "Google Drive - Personal" \
+)
+
+# Mount Remotes
+for i in "${remote_array[@]}"
+do
+    local_path="$local_clone_dir"/"$i"
+    mkdir -p "$local_path"
+    rclone --vfs-cache-mode writes mount "$i": "$local_path" &
+done
+
