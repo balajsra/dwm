41958715d4b39869c470731ca31a30006462d710
Sravan Balaji <balajsra@umich.edu>
2020-07-31 23:14:10 -0400

    Apply movestack patch
    - Add keybinds to switch window between master and slaves
    - Add movestack.c

--- config.h
+++ config.h
@@ -1,5 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
+/* includes */
+#include "movestack.c"
+
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -80,6 +83,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      		incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      		setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      		setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_j,      		movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      		movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_Return, 		zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    		view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      		killclient,     {0} },
