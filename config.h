8fe17768f750921eaf17ae13840f5ec74d617457
Sravan Balaji <balajsra@umich.edu>
2020-08-05 22:20:12 -0400

    Nerd Font, Icons, & Size Changes
    - Change font to NotoSans Nerd Font
    - Change font size to 10
    - Change tag icons and add subscript numbers

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     	/* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12", "fontawesome:size=12" };
+static const char *fonts[]          = { "NotoSans Nerd Font:size=10" };
 static const char col_gray1[]       = "#282a36"; /* background color */
 static const char col_gray2[]       = "#6272a4"; /* inactive window border color */
 static const char col_gray3[]       = "#f8f8f2"; /* font color */
@@ -26,7 +26,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { " ₁", "龎 ₂", " ₃", " ₄", "爵 ₅", " ₆", " ₇", " ₈", " ₉" };
 
 static const Rule rules[] = {
 	/* xprop(1):
