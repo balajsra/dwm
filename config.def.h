9253bcdb53bcf0ea2474d09d872554bbfb1029de
Sravan Balaji <balajsra@umich.edu>
2020-11-24 08:26:18 -0500

    Dracula Theme
    - Change colors and gap sizes
    - Change quit button to Mod+Ctrl+Shift+q

--- config.def.h
+++ config.def.h
@@ -1,26 +1,25 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
 static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
 static const char *alttrayname      = "tray";    /* Polybar tray instance name */
 static const char *altbarcmd        = "$HOME/bar.sh"; /* Alternate bar launch command */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "NotoSans Nerd Font:size=10" };
+static const char col_gray1[]       = "#282a36"; /* background color */
+static const char col_gray2[]       = "#44475a"; /* inactive window border color */
+static const char col_gray3[]       = "#f8f8f2"; /* font color */
+static const char col_gray4[]       = "#282a36"; /* current tag and current window font color */
+static const char col_cyan[]        = "#bd93f9"; /* Top bar second color and active window border color */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -64,9 +63,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     iscentered   isfloating   monitor */
+	{ "origin",   NULL,       NULL,       0,            1,           1,           -1 },
 	// { "Gimp",     NULL,       NULL,       0,            0,           1,           -1 },
 	// { "Firefox",  NULL,       NULL,       1 << 8,       0,           0,           -1 },
-	{ "origin",   NULL,       NULL,       0,            1,           1,           -1 },
 };
 
 /* layout(s) */
@@ -197,7 +196,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                            6)
 	TAGKEYS(                        XK_8,                            7)
 	TAGKEYS(                        XK_9,                            8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,                 {0} },
+	{ MODKEY|ControlMask|ShiftMask, XK_q,                       quit,                 {0} },
 };
 
 /* button definitions */
