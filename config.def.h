9f219c6a08a0cdfd587147ebb3917a611d19ae0e
Sravan Balaji <balajsra@umich.edu>
2020-11-24 10:43:39 -0500

    Swall Fix & Make Clean
    - Fix alacritty swallow rules
    - Remove config.h on make clean

--- config.def.h
+++ config.def.h
@@ -65,10 +65,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class       instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "origin",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "alacritty", NULL,     NULL,           0,         0,          1,           0,        -1 },
-	// { "Gimp",      NULL,     NULL,           0,         1,          0,           0,        -1 },
-	// { "Firefox",   NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ NULL,        NULL,     "Origin",       0,         1,          0,           0,        -1 },
+	{ "Alacritty", NULL,     NULL,           0,         0,          0,           1,        -1 },
 	{ NULL,        NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
