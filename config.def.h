edb476243431e62381caf51b739d67d3b39031e3
Sravan Balaji <balajsra@umich.edu>
2020-12-07 21:56:34 -0500

    Jonaburg Picom
    - Show outer gap even when there is only one window
    - Change picom autostart to use "--experimental-backend" flag

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@ static       unsigned int gappoh    = 10;       /* horiz outer gap between windo
 static       unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int noborder           = 0;        /* 1 means no border when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
@@ -42,7 +42,7 @@ static const char *const autostart[] = {
     "flameshot", NULL,                                                   // Flameshot Screenshot Tool
 	"xfce4-power-manager", NULL,                                         // XFCE4 Power Manager
     // Background Processes
-    "picom", "--config", "/home/sravan/.config/picom/picom.conf", NULL,  // Picom Compositor
+    "picom", "--experimental-backend", "--config", "/home/sravan/.config/picom/picom.conf", NULL,  // Picom Compositor
     "/usr/lib/xfce4/notifyd/xfce4-notifyd", NULL,                        // Xfce Notification Daemon
     "greenclip", "daemon", NULL,                                         // Greenclip Clipboard Manager
     "redshift", "-x", NULL,                                              // Reset redshift display gamma
